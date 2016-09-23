0deaa5b4b5d038f4022f30e415bd781a1309d6b1
trautlein <hans@trautlein.com>
2016-09-23 15:34:03 -0700

    Added underscore lib, started trying to add enemies

--- client/watchout.js
+++ client/watchout.js
@@ -17,3 +17,27 @@ var axes = {
   x: d3.scale.linear().domain([0, 100]).range([0, boardOptions.width]),
   y: d3.scale.linear().domain([0, 100]).range([0, boardOptions.height])
 };
+
+var rangeEvil = _.range(0, 10);
+
+var makeEvils = _.map(rangeEvil, function (i) {
+  var obj = {};
+
+  obj.id = i;
+  obj.x = Math.floor(Math.random() * 100);
+  obj.y = Math.floor(Math.random() * 100);
+  
+  return obj;
+});
+
+debugger;
+var evils = d3.select('circle.enemy')
+              .data(makeEvils, function(d) { return d.id; });
+
+evils.enter()
+     .append('svg:circle')
+       .attr('class', 'evil')
+       .attr('cx', function(d) { return axes.x(d.x); })
+       .attr('cy', function(d) { return axes.y(d.y); })
+       .attr('r', 10);
+
