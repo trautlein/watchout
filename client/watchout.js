0d585129e347e940bb31f6a8b838a41713107de2
Hans Trautlein <hans@trautlein.com>
2016-09-23 16:17:53 -0700

    fixed index.html link to css file and lightened bg

--- client/watchout.js
+++ client/watchout.js
@@ -13,7 +13,7 @@ d3.select('.board').append('svg')
   .attr('height', boardOptions.height)
   .attr('width', boardOptions.width);
 
-//
+
 var axes = {
   x: d3.scale.linear().domain([0, 100]).range([0, boardOptions.width]),
   y: d3.scale.linear().domain([0, 100]).range([0, boardOptions.height])
@@ -23,21 +23,22 @@ var evils = _.map(_.range(0, boardOptions.numberEnemies), function (i) {
   var obj = {};
 
   obj.id = i;
-  obj.x = Math.floor(Math.random() * 100);
-  obj.y = Math.floor(Math.random() * 100);
+  obj.x = Math.random() * 100;
+  obj.y = Math.random() * 100;
   
   return obj;
 });
 
 
-d3.select(".gameboard").selectAll("svg")
-  .data([{id: 0, x: 30, y: 40}, {id: 1, x: 80, y: 60}])
-  .enter().append('svg:circle')
-  .attr('cx', function(d) { return axes.x(d.x); })
-  .attr('cy', function(d) { return axes.x(d.y); })
+d3.select('svg').selectAll('circle')
+  .data(evils)
+  .enter().append('circle')
+  .attr('cx', function(d) { return Math.floor(axes.x(d.x)); })
+  .attr('cy', function(d) { return Math.floor(axes.x(d.y)); })
   .attr('radius', 10)
-  .style('color', 'black')
-  .style('z-index', 1000);
+  .style('fill', 'yellow');
+  // .attr('fill', 'black');
+
 
 // debugger;
 // var evils = d3.select('circle.enemy')
