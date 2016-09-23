7e8f244a69c0528ec26a15caf70df5e4321f69f9
trautlein <hans@trautlein.com>
2016-09-23 14:57:56 -0700

    Added gameboard (axis and related options as well)

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,19 @@
 // start slingin' some d3 here.
+
+var boardOptions = {
+  height: 400,
+  width: 500
+};
+
+
+// svg that everything operates in
+d3.select('.board').append('svg')
+  .classed('gameboard', true)
+  .attr('height', boardOptions.height)
+  .attr('width', boardOptions.width);
+
+//
+var axes = {
+  x: d3.scale.linear().domain([0, 100]).range([0, boardOptions.width]),
+  y: d3.scale.linear().domain([0, 100]).range([0, boardOptions.height])
+};
