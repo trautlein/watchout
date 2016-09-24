cdc4fbdcfe7bab6b9e5b48a0ebbb66517c8a18c7
trautlein <hans@trautlein.com>
2016-09-23 20:02:43 -0700

    DEAR GOD IT MOVES

--- client/watchout.js
+++ client/watchout.js
@@ -1,66 +1,76 @@
-// start slingin' some d3 here.
-
 var boardOptions = {
   height: 400,
   width: 500,
   numberEnemies: 20
 };
 
-
-// svg that everything operates in
 d3.select('.board').append('svg')
   .classed('gameboard', true)
   .attr('height', boardOptions.height)
   .attr('width', boardOptions.width);
-
 
 var axes = {
   x: d3.scale.linear().domain([0, 100]).range([0, boardOptions.width]),
   y: d3.scale.linear().domain([0, 100]).range([0, boardOptions.height])
 };
 
+// map evils to 
 var evils = _.map(_.range(0, boardOptions.numberEnemies), function (i) {
-  var obj = {};
-
-  obj.id = i;
-  obj.x = Math.random() * 100;
-  obj.y = Math.random() * 100;
-  
+  var obj = {
+    id: i,
+    x: Math.random() * 100,
+    y: Math.random() * 100
+  };
   return obj;
 });
 
-// var move = d3.select('svg').selectAll('circle')
-//   .data(evils)
-//   .enter().append('circle')
-//   .attr('cx', function(d) { return Math.floor(axes.x(d.x)); })
-//   .attr('cy', function(d) { return Math.floor(axes.x(d.y)); })
-//   .attr('r', 7)
-//   .style('fill', 'red')
-//   .transition()
-//   .duration(3000);
-
-
-var update = function (d) {
-  d3.select('svg').selectAll('circle')
-               .data(evils)
-               .enter().append('circle')
-               .style('fill', 'red')
-               .transition()
-               .duration(2000)
-               .attr('cx', function(d) { return Math.floor(axes.x(d.x)); })
-               .attr('cy', function(d) { return Math.floor(axes.x(d.y)); })
-               .attr('r', 7);
+var updateArray = function() {
+  var newArray = [];
+  for (var i = 0; i < boardOptions.numberEnemies; i++) {
+    newArray.push({
+      id: i,
+      x: Math.random() * 100,
+      y: Math.random() * 100
+    });
+  }
+  return newArray;
 };
 
 
-// var xtime = _.map(_.range(0, boardOptions.numberEnemies), function (i) {
-//   var obj = {};
+var start = function (d) {
+  console.log(d);
+  d3.select('svg').selectAll('circle')
+                  .data(d)
+                  .enter().append('circle')
+                  .style('fill', 'red')
+                  .attr('r', 7)
+                  .attr('cx', function(d) { return Math.floor(axes.x(d.x)); })
+                  .attr('cy', function(d) { return Math.floor(axes.x(d.y)); });
+};
 
-//   obj.id = i;
-//   obj.x = Math.random() * 100;
-//   obj.y = Math.random() * 100;
-  
-//   return obj;
-// });
+var update = function (d) {
+  console.log(d);
+  d3.select('svg').selectAll('circle')
+                  .data(d)
+                  .transition()
+                  .duration(2000)
+                  .attr('cx', function(d) { return Math.floor(axes.x(d.x)); })
+                  .attr('cy', function(d) { return Math.floor(axes.y(d.y)); });
+};
 
-update(evils);
+start(evils);
+update(updateArray());
+setInterval(function() { 
+  update(updateArray());
+}, 2000);
+
+// setInterval(update(function() { updateArray() }), 4000);
+
+
+
+
+
+
+
+
+
