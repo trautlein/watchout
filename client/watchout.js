557b2d73f43f08c82df15bf45bd1604b29fff68c
Hans Trautlein <hans@trautlein.com>
2016-09-23 17:30:55 -0700

    added transition, but it's not transitioning!

--- client/watchout.js
+++ client/watchout.js
@@ -29,17 +29,38 @@ var evils = _.map(_.range(0, boardOptions.numberEnemies), function (i) {
   return obj;
 });
 
-console.log(evils);
-
-var move = d3.select('svg').selectAll('circle')
-  .data(evils)
-  .enter().append('circle')
-  .attr('cx', function(d) { return Math.floor(axes.x(d.x)); })
-  .attr('cy', function(d) { return Math.floor(axes.x(d.y)); })
-  .attr('r', 7)
-  .style('fill', 'red')
-  .transition()
-  .duration(3000);
+// var move = d3.select('svg').selectAll('circle')
+//   .data(evils)
+//   .enter().append('circle')
+//   .attr('cx', function(d) { return Math.floor(axes.x(d.x)); })
+//   .attr('cy', function(d) { return Math.floor(axes.x(d.y)); })
+//   .attr('r', 7)
+//   .style('fill', 'red')
+//   .transition()
+//   .duration(3000);
 
 
+var update = function (d) {
+  d3.select('svg').selectAll('circle')
+               .data(evils)
+               .enter().append('circle')
+               .style('fill', 'red')
+               .transition()
+               .duration(2000)
+               .attr('cx', function(d) { return Math.floor(axes.x(d.x)); })
+               .attr('cy', function(d) { return Math.floor(axes.x(d.y)); })
+               .attr('r', 7);
+};
+
+
+// var xtime = _.map(_.range(0, boardOptions.numberEnemies), function (i) {
+//   var obj = {};
+
+//   obj.id = i;
+//   obj.x = Math.random() * 100;
+//   obj.y = Math.random() * 100;
   
+//   return obj;
+// });
+
+update(evils);
